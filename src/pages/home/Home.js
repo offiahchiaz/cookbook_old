5a6977841f89cfd09860c031b5a86e78bab367a4
offiahchiaz <offiahchiaz@gmail.com>
2022-02-10 14:56:34 +0100

    Added useFetch hook to Home component for getting lsit of recipies

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,21 @@
+import { useFetch } from '../../hook/useFetch';
+
 // Style 
 import './Home.css'
 
 
 export default function Home() {
+  const { data: recipes, isPending, error } = useFetch('http://localhost:3000/recipes')
+
   return (
     <div>
-      
+      { error && <p className='error'>{error}</p> }
+      { isPending && <p className="loading">Loading...</p> }
+      {
+        recipes && recipes.map(recipe => (
+          <h2 key={recipe.id}>{recipe.title}</h2>
+        ))
+      }
     </div>
   );
 }
