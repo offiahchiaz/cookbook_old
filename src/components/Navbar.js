4de032edc55976608902bd3059ce4b0b39f8d6ca
offiahchiaz <offiahchiaz@gmail.com>
2022-02-14 10:26:06 +0100

    Updated the nav for displayig diffrent theme colors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
 import { Link } from 'react-router-dom';
 import Searchbar from './Searchbar';
+import { useTheme } from '../hook/useTheme';
+
 
 // Styels
 import './Navbar.css'
 
 
 
+
+
 export default function Navbar() {
+
+  const { color, changeColor } = useTheme()
+
   return (
-    <div className='navbar'>
-      <nav>
+    <div className='navbar' style={{ background: color }}>
+      <nav onClick={() => changeColor('pink') }>
         <Link to="/" className='brand'>
           <h1>Cooking Ninja</h1>
         </Link>
