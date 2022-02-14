dd820810334ee9813735a96c821c92ff452ddb5f
offiahchiaz <offiahchiaz@gmail.com>
2022-02-14 13:28:45 +0100

    Aadde dark mode feature to Recipe component

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
-
 import { useFetch } from '../../hook/useFetch';
+import { useTheme } from '../../hook/useTheme';
+
 
 // Style
 import './Recipe.css'
@@ -9,9 +10,10 @@ export default function Recipe() {
 
   const { id } = useParams()
   const { data: recipe, isPending, error } = useFetch(`http://localhost:3000/recipes/${id}`)
+  const { mode } = useTheme
 
   return (
-    <div className='recipe'>
+    <div className={`recipe ${mode}`}>
       { error && <p className='error'>{error}</p> }
       { isPending && <p className="loading">Loading...</p> }
       { console.log(recipe) }
